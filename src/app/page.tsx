ef82329ce0c48b811e541b72d31ba660587c8c48
Phil Dakin <phil.dakin.1@gmail.com>
2023-05-15 16:02:19 -0400

    Basic UI functionality working.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,114 @@
+"use client";
+
+import { List, Fade, Pagination, AppBar, Box, Button, CircularProgress, Paper, TextField, Typography } from "@mui/material"
 import Image from 'next/image'
+import { useTheme } from '@mui/material/styles';
+import { Dispatch, SetStateAction, useState } from "react";
+import usePagination from "@mui/material/usePagination/usePagination";
+
+// TODO (pdakin): Things you did in Figma to make it work on first pass maybe not always the best.
+// Is there a way to write this without so many <Paper/> layers?
+
+enum State {
+  WELCOME,
+  ENTRY,
+  LOADING,
+  DISPLAY
+}
+
+type MainComponentProps = {
+  setState: Dispatch<SetStateAction<State>>
+}
+
+function getMainComponent(state: State, setState: Dispatch<SetStateAction<State>>) {
+  switch (state) {
+    case State.WELCOME: {
+      return <Welcome setState={setState} />
+    }
+    case State.ENTRY: {
+      return <Entry setState={setState} />
+    }
+    case State.LOADING: {
+      return <Loading setState={setState} />
+    }
+    case State.DISPLAY: {
+      return <Display setState={setState} />
+    }
+  }
+}
+
+function Welcome({ setState }: MainComponentProps) {
+  const theme = useTheme();
+  return (
+    <Fade in={true} timeout={1200}>
+      <Paper sx={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Paper sx={{ height: '55%', width: '40%' }} elevation={0}>
+          <Typography variant='h1' color="secondary" component={"span"}>Learn faster</Typography>
+          <Typography variant='h1' component={"span"}> with better summaries.</Typography>
+          <Typography sx={{ marginY: 1 }} color="text.hint" variant='h6'>Doctrine provides comprehensive summaries of the information you need to know, allowing you to focus more on what matters.</Typography>
+          <Button onClick={() => setState(State.ENTRY)} sx={{ marginY: 1 }} style={{ backgroundColor: theme.palette.secondary.main }} color="primary">TRY IT OUT</Button>
+        </Paper>
+      </Paper>
+    </Fade>
+  )
+}
+
+function Entry({ setState }: MainComponentProps) {
+  const theme = useTheme();
+  return <Fade in={true} timeout={1200}>
+    <Paper sx={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Paper sx={{ height: '55%', width: '50%' }} elevation={0}>
+        <Box>
+          <TextField multiline fullWidth minRows={3} maxRows={15} variant="filled" placeholder="Enter your corpus here..." />
+        </Box>
+        {/* TODO (pdakin): Going to need to do something with the text here... */}
+        <Button onClick={() => setState(State.LOADING)} sx={{ marginY: 1 }} style={{ backgroundColor: theme.palette.secondary.main }} color="primary">SUMMARIZE</Button>
+      </Paper>
+    </Paper>
+  </Fade>
+}
+
+function Loading({ setState }: MainComponentProps) {
+  // TODO (pdakin): Use API request success as transition here.
+  setInterval(
+    () => {
+      setState(State.DISPLAY)
+    }, 2500
+  )
+  return <Fade in={true} timeout={1200}>
+    <Paper sx={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <CircularProgress color="secondary" size={100} />
+    </Paper>
+  </Fade>
+}
+
+function Display({ setState }: MainComponentProps) {
+  // TODO (pdakin): Support custom labels in the pagination list.
+  const [page, setPage] = useState(1);
+  return <Fade in={true} timeout={1200}>
+    <Paper sx={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+      <Paper sx={{ height: '100%', width: '50%', display: 'flex', alignItems: 'center', flexDirection: 'column' }} elevation={0}>
+        <Pagination onChange={(_, p) => { setPage(p) }} count={3} shape="rounded" variant="outlined" />
+        <Typography sx={{ marginY: 3 }} >#{page} - Note that the Pagination page prop starts at 1 to match the requirement of including the value in the URL, while the TablePagination page prop starts at 0 to match the requirement of zero-based JavaScript arrays that comes with rendering a lot of tabular data.</Typography>
+      </Paper>
+    </Paper>
+  </Fade>
+}
 
 export default function Home() {
+  const [state, setState] = useState(State.WELCOME);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing. Woah look, you did it!&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Paper sx={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column' }} square={true}>
+      <AppBar sx={{ height: 120, flexDirection: 'row' }} position={'relative'} elevation={0}>
+        <Box sx={{ 'height': '75%', 'width': 32, 'position': 'relative', 'marginY': 1.9 }}>
+          <Image src='/doctrine_logo.png' fill={true} alt="" />
+        </Box>
+        <Box sx={{ 'height': '100%', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <Typography variant='h3'>Doctrine</Typography>
+        </Box>
+      </AppBar>
+      {/* TODO (pdakin): Animate transitions. */}
+      {getMainComponent(state, setState)}
+    </Paper >
   )
 }
---
+++ src/app/theme.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#ffffff',
+        },
+        secondary: {
+            main: '#7b1fa2',
+        },
+    },
+});
+
+export default theme;
