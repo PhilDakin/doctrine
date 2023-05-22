7e3363b19f2e0591d5dfa0aa96425e544ed027fb
Phil Dakin <phil.dakin.1@gmail.com>
2023-05-22 18:32:05 -0400

    Revert to using OpenAI pending long-term performance fix.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import CustomPagination from "./CustomPagination";
 import Image from "next/image";
 import { useTheme } from "@mui/material/styles";
 import { Dispatch, SetStateAction, useState } from "react";
-import { summarize } from "./models/huggingface/Llm";
+import { summarize } from "./models/openai/Llm";
 
 // TODO (pdakin): Things you did in Figma to make it work on first pass maybe not always the best.
 // Is there a way to write this without so many <Paper/> layers?
